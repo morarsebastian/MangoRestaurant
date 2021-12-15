b5eb683aa047e744784d11b8fcd73acd5212abf3
Sebastian Morar <sebastian.morar@aily-labs.com>
2021-12-15 14:33:56 +0200

    Set up Shopping Cart API

---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public static class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
